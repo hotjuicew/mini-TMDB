7e7c1d214ee4e5629f4dccc451674f714acf487c
hotjuicew <hotjuicew@outlook.com>
2022-09-12 00:49:46 +0800

    add:t添加搜索栏

--- src/views/home/cpns/HomeHeader.vue
+++ src/views/home/cpns/HomeHeader.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="home-header">
-    <el-input v-model="input" placeholder="Please input" />
-    <Search style="width: 1em; height: 1em; margin-right: 8px" />
+    <div class="demo-input-size">
+      <el-input
+          v-model="input"
+          class="w-50 m-2"
+          size="large"
+          placeholder="Please Input"
+      />
+      <button @click="clickSearch(input)"><el-icon><Search /></el-icon></button>
   </div>
+    </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { Search } from '@element-plus/icons-vue'
 const input = ref('')
+const clickSearch = (input) => {
+  console.log(input.trim())
+}
 </script>
 
 <style lang="less" scoped>
