cd5f690724fa4b32618ad45303778156c6466d33
hotjuicew <hotjuicew@outlook.com>
2022-09-15 16:14:39 +0800

    add: 改了一些list的样式

--- src/stores/home.js
+++ src/stores/home.js
@@ -2,7 +2,29 @@ import { defineStore } from "pinia";
 
 const useHomeStore = defineStore("home", {
     state: () => ({
-        searchList:[]
+        searchList:[
+            {
+                "sid": "1295038",
+                "name": "哈利·波特与魔法石",
+                "rating": "0",
+                "img": "https://img2.doubanio.com/view/photo/s/public/p2614949805.jpg",
+                "year": "2001"
+            },
+            {
+                "sid": "1291544",
+                "name": "哈利·波特与阿兹卡班的囚徒",
+                "rating": "0",
+                "img": "https://img2.doubanio.com/view/photo/s/public/p1910812549.jpg",
+                "year": "2004"
+            },
+            {
+                "sid": "1296996",
+                "name": "哈利·波特与密室",
+                "rating": "0",
+                "img": "https://img2.doubanio.com/view/photo/s/public/p2440298618.jpg",
+                "year": "2002"
+            }
+        ]
 
     }),
     actions: {
--- src/views/list/ListView.vue
+++ src/views/list/ListView.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="Title">
-   <h2>List</h2>
+  <div class="list-view">
     <div class="list">
+      <header class="search-title">相关豆瓣内容:</header>
       <div class="item" v-for="item in newList.value">
         <div class="left">
           <h3 class="title">{{ item.name }}</h3>
+          <div class="year">{{item.year}}</div>
         </div>
         <div class="right">
-          <img :src="item.img" :alt="item.name+'图片'">
+          <img class="search-img" :src="item.img" :alt="item.name+'图片'">
         </div>
       </div>
     </div>
@@ -28,8 +29,86 @@ const newList = computed(() => {
   }
   return  list
 })
+
+// import { getCurrentInstance } from 'vue'
+// const instance = getCurrentInstance();
+// instance?.proxy?.$forceUpdate();
 </script>
 
 <style lang="less" scoped>
-
+.list-view{
+  display: flex;
+  justify-content: center;
+}
+.list{
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  width: 50vw;
+  .search-title{
+    align-self:flex-start;
+  }
+  .search-title{
+    font-size: 1.2rem;
+  }
+  .item{
+    display: flex;
+    justify-content: space-between;
+    width: 50vw;
+    margin-top: 1rem;
+    .left{
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      .year{
+        margin-top: 0.5rem;
+      }
+    }
+    .search-img{
+      width: 10vw;
+    }
+  }
+  //.item:after {
+  //  content: '\0020';
+  //  clear: both;
+  //  display: block;
+  //}
+}
 </style>
+<!--<style lang="less" scoped>-->
+<!--.list-view{-->
+<!--  display: flex;-->
+<!--  justify-content: center;-->
+<!--}-->
+<!--.list{-->
+<!--  display: flex;-->
+<!--  flex-direction: column;-->
+<!--  align-items:center;-->
+<!--  width: 50vw;-->
+<!--  .search-title{-->
+<!--    align-self:flex-start;-->
+<!--  }-->
+<!--  .search-title{-->
+<!--    font-size: 1.2rem;-->
+<!--  }-->
+<!--  .item{-->
+<!--    display: flex;-->
+<!--    justify-content: space-between;-->
+<!--    width: 50vw;-->
+<!--    margin-top: 1rem;-->
+<!--    .left{-->
+<!--      display: flex;-->
+<!--      flex-direction: column;-->
+<!--      align-items: flex-start;-->
+<!--    }-->
+<!--    .search-img{-->
+<!--      width: 10vw;-->
+<!--    }-->
+<!--  }-->
+<!--  .item:after {-->
+<!--    content: '\0020';-->
+<!--    clear: both;-->
+<!--    display: block;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
