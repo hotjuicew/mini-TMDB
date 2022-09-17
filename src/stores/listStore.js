7caf8e7ce6e332617e9f3624e9830fc141c7850f
hotjuicew <hotjuicew@outlook.com>
2022-09-17 23:01:34 +0800

    chore：格式化代码

--- src/stores/listStore.js
+++ src/stores/listStore.js
@@ -1,91 +1,77 @@
 import {defineStore} from "pinia";
 import axiosInstance from "@/servers/request";
 import axios from "axios";
-axiosInstance.all=axios.all
-axiosInstance.spread=axios.spread
+
+axiosInstance.all = axios.all
+axiosInstance.spread = axios.spread
 const useListStore = defineStore("list", {
     state: () => ({
-        input:'',
+        input: '',
         tvList: [],
-        movieList:[],
-        personList:[],
-        collectionList:[],
+        movieList: [],
+        personList: [],
+        collectionList: [],
     }),
-    getters: {
-       // getSearchList(){
-       //      this.getSearchMovie().then(res => {
-       //          this.movieList = res.data.results
-       //      })
-       //      this.getSearchTV().then(res => {
-       //          this.tvList = res.data.results
-       //      })
-       //      this.getSearchPerson().then(res => {
-       //          this.personList = res.data.results
-       //      })
-       //     this.getSearchCollection().then(res => {
-       //          this.collectionList = res.data.results
-       //      })
-       //  },
-    },
+    getters: {},
 
 
     actions: {
         //axios.all是一个静态方法
-        getSearchList(){
+        getSearchList() {
             Promise.all([
                 this.getSearchTV(),
                 this.getSearchMovie(),
                 this.getSearchPerson(),
                 this.getSearchCollection()
-            ]).then(axiosInstance.spread((tvRes, movieRes,personRes,collectionRes) => {
-    this.movieList = tvRes.data.results
-    this.tvList = movieRes.data.results
-    this.personList = personRes.data.results
-    this.collectionList = collectionRes.data.results
+            ]).then(axiosInstance.spread((tvRes, movieRes, personRes, collectionRes) => {
+                this.movieList = tvRes.data.results
+                this.tvList = movieRes.data.results
+                this.personList = personRes.data.results
+                this.collectionList = collectionRes.data.results
             }))
         },
 
 
         //search TV
-         getSearchTV(){
+        getSearchTV() {
             return axiosInstance.get('/search/tv', {
                 params: {
                     api_key: '6575ea93f20a3172600a4cfb722e23ce',
-                    language:'zh',
+                    language: 'zh',
                     query: this.input,
                     page: 1,
-                    include_adult:false
+                    include_adult: false
                 }
             })
         },
-         getSearchMovie(){
+        getSearchMovie() {
             return axiosInstance.get('/search/movie', {
                 params: {
                     api_key: '6575ea93f20a3172600a4cfb722e23ce',
-                    language:'zh',
+                    language: 'zh',
                     query: this.input,
                     page: 1,
-                    include_adult:false
+                    include_adult: false
                 }
             })
         },
-         getSearchPerson(){
+        getSearchPerson() {
             return axiosInstance.get('/search/person', {
                 params: {
                     api_key: '6575ea93f20a3172600a4cfb722e23ce',
-                    language:'zh',
+                    language: 'zh',
                     query: this.input,
                     page: 1,
-                    include_adult:false
+                    include_adult: false
                 }
             })
         },
 
-         getSearchCollection(){
+        getSearchCollection() {
             return axiosInstance.get('/search/collection', {
                 params: {
                     api_key: '6575ea93f20a3172600a4cfb722e23ce',
-                    language:'zh',
+                    language: 'zh',
                     query: this.input,
                     page: 1,
                 }
